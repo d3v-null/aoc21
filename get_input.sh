7b742f553dd294998a687566b6cb72747fe22366
derwentx <derwentx@gmail.com>
2021-12-16 12:40:32 +0800

    Update license

--- get_input.sh
+++ get_input.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export c=session=53616c7465645f5fdc6a11fbbd01401e02c6c87c265298a2dc797ae1813a4f64597eb39b7be080e24b4e34c0df02f73e
+export c=session=...
 curl -b$c https://adventofcode.com/2021/day/$1/input > input.txt
